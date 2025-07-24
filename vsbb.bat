c08c9473a3df052a88769952857db003e1080fd2
gamej4194 <gamej4194@gmail.com>
2025-07-24 19:45:31 -0400

    Update vsbb.bat

--- vsbb.bat
+++ vsbb.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 rem NAME OF THE WORLD YOU WICH TO BACKUP
-set savename=SAVE-NAME
+set savename=121
 
 rem WHERE TO SAVE
 set backupFolder=C:\vsbackup
@@ -10,23 +10,24 @@ set backupFolder=C:\vsbackup
 rem HOW MANY BACKUPS
 set BackupNumber=5
 
-set "sourceFile=%appdata%\VintagestoryData\Saves\%savename%.vcdbs"
-set "lastBackup=%backupFolder%\%savename%.bak1"
-fc %sourceFile% %lastBackup% >nul
-if errorlevel 1 (
-    echo Files are different.
-) else (
-	exit /b
-)
-tasklist /FI "IMAGENAME eq Vintagestory.exe" 2>NUL | find /I "Vintagestory.exe" >NUL
-if "%ERRORLEVEL%"=="0" (
-    echo Vintagestory.exe is running
-) else (
-	set baseName=%savename%
-	if exist "%backupFolder%\%savename%.bak%BackupNumber%" del "%backupFolder%\%savename%.bak%BackupNumber%"	
-	for /l %%i in (%BackupNumber%,-1,1) do (
-		set /a o=%%i+1
-		if exist "%backupFolder%\%savename%.bak%%i" ren "%backupFolder%\%savename%.bak%%i" "%savename%.bak!o!"
+set "folderPath=%appdata%\VintagestoryData\Saves"
+
+	set "sourceFile=%appdata%\VintagestoryData\Saves\%savename%.vcdbs"
+	set "lastBackup=%backupFolder%\%savename%.bak1"
+	fc %sourceFile% %lastBackup% >nul
+	if errorlevel 1 (
+	tasklist /FI "IMAGENAME eq Vintagestory.exe" 2>NUL | find /I "Vintagestory.exe" >NUL
+	if "%ERRORLEVEL%"=="0" (
+		echo Vintagestory.exe is running
+	) else (
+
+		set baseName=%savename%
+		if exist "%backupFolder%\%savename%.bak%BackupNumber%" del "%backupFolder%\%savename%.bak%BackupNumber%"	
+		for /l %%i in (%BackupNumber%,-1,1) do (
+			set /a o=%%i+1
+			if exist "%backupFolder%\%savename%.bak%%i" ren "%backupFolder%\%savename%.bak%%i" "%savename%.bak!o!"
+		)
+	copy /Y "%sourceFile%" "%backupFolder%\%savename%.bak1"
 	)
-copy /Y "%sourceFile%" "%backupFolder%\%savename%.bak1"
-)
+	)
+
